ea20aa0651daa51dedf83900b3547adb6e4ee1e7
Gustin <florian.gustin.business@gmail.com>
2021-10-25 12:16:20 +0200

    feat : add

---
+++ src/test/java/com/example/esgi_jenkins/EsgiJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esgi_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsgiJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
